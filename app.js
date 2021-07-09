81a8862c9e74210a50ed0d417213e00b75fcc5ac
tanishqchugh2002 <tanishqchugh85@gmail.com>
2021-07-10 00:14:10 +0530

    Added heroku requirements and gitignore

--- app.js
+++ app.js
@@ -84,6 +84,12 @@ app.post("/contact",function(req,res){
     res.render("thanks");
 })
 
-app.listen(3000,function(){
-    console.log("Server started succesfully on port 3000");
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function () {
+  console.log("Server has started succesfully");
+});
